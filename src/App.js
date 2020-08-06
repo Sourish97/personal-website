114f06cbbaaf0e9c6cf8853c3f1e7fe7896be28c
Sourish97 <sourishvaghulade@gmail.com>
2020-08-06 01:17:44 -0400

    Complete url

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'resumeData.json',
+      url:'https://sourish97.github.io/personal-website/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
