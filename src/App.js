3a8b75532d20a49f7333c6d302767f9912e44995
Sourish97 <sourishvaghulade@gmail.com>
2020-08-06 01:10:34 -0400

    Second attempt

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:process.env.PUBLIC_URL+'/resumeData.json',
+      url:'/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
